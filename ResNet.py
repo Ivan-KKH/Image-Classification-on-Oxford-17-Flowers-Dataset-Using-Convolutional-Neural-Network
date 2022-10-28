a51d0189f2e475a737533f69b073dfd8112beac2
Ivan-KKH <kwokkahomail@gmail.com>
2022-10-28 11:07:04 +0800

    removed duplicated preprocesing after split aug

--- ResNet.py
+++ ResNet.py
@@ -75,33 +75,21 @@ plt.ion()   # interactive mode
 #    and extract it to the current directory.
 # 
 # 
-'''
-transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-'''
+
 # %%
 # Data augmentation and normalization for training
 # Just normalization for validation
 data_transforms = {
     
     'train': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),    
     'val': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ]),    
     'test': transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
